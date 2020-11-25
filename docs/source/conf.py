43a53f2989ecc756bb4abc2a484c45c2e05f147a
doclockh <chris@lockhartlab.org>
2020-11-25 13:43:28 -0800

    changing theme to alabaster for fun

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -79,7 +79,7 @@ viewcode_follow_imported_members = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
+html_theme = 'alabaster'  #'nature'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
