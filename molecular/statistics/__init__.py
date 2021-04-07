d35cfe464c44aa3f12135120502ef70ac6c91622
Chris Lockhart <chris@lockhartlab.org>
2021-04-07 11:59:53 -0700

    converting xyz to float on pdb read

--- molecular/statistics/__init__.py
+++ molecular/statistics/__init__.py
@@ -8,7 +8,29 @@ __all__ = [
     'Block',
     'block_average',
     'block_error',
+    'normalize',
     'sem_block',
     'sem_tcorr',
-    'tcorr'
+    'standardize',
+    'tcorr',
 ]
+
+
+# TODO find me a home
+import numpy as np
+import pandas as pd
+def normalize(a):
+    if isinstance(a, (pd.DataFrame, pd.Series)):
+        a = a.div(a.abs().sum(axis=0), axis=1)  # noqa
+
+    elif isinstance(a, (list, np.ndarray)):
+        a = a / np.sum(np.abs(a))
+
+    else:
+        assert AttributeError('cannot normalize')
+
+    return a
+
+
+def standardize(a):
+    raise NotImplementedError
