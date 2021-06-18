62a16a55093547ceb4c0da743d23c347f2055f95
Chris Lockhart <chris@lockhartlab.org>
2021-06-18 15:52:57 -0700

    histogram now can take DataFrames

--- molecular/analysis/histogram.py
+++ molecular/analysis/histogram.py
@@ -1,15 +1,24 @@
-
 import numpy as np
+import pandas as pd
 
 
 class Histogram:
-    def __init__(self, bins, counts, weights=None):
-        self._bins = bins
-        self._counts = counts
+    def __init__(self, data, weights=None):
+        self._data = data
+
         # FIXME for non-ihist, the width will impact the mass function
         if weights is None:
             weights = 1.
-        self._probs = counts / np.sum(counts)  # pmf
+
+        # self._probs = counts / np.sum(counts)  # pmf
+
+    @property
+    def counts(self):
+        return self._data
+
+    @property
+    def pmf(self):
+        return self._data / self._data.sum(axis=0)
 
     def plot(self):
         import uplot as u
@@ -18,15 +27,14 @@ class Histogram:
     # FIXME when count = False and probability = False, this will break because there will be a pandas index with no
     #  other content.
     def to_frame(self, count=True, probability=False):
-        import pandas as pd
-
-        df = pd.DataFrame({'bin': self._bins})
-        if count:
-            df['count'] = self._counts
-        if probability:
-            df['probability'] = self._probs
-
-        return df.set_index('bin')
+        # df = pd.DataFrame({'bin': self._bins})
+        # if count:
+        #     df['count'] = self._counts
+        # if probability:
+        #     df['probability'] = self._probs
+        #
+        # return df.set_index('bin')
+        return self._data
 
 
 def hist(a, bins):
@@ -53,7 +61,7 @@ def ihist(a, width, right=False):
 
     Parameters
     ----------
-    a : 1D array
+    a : array
         Collection of values to turn into histogram.
     width : number
         The interval that should be binned.
@@ -68,12 +76,20 @@ def ihist(a, width, right=False):
 
     # Take `a` and get binned values
     func = np.floor if not right else np.ceil
-    a = np.copy(func(a / width) * width)
+    a_binned = np.copy(func(a / width) * width)
 
-    # Create Histogram object and return
-    return Histogram(*np.unique(a, return_counts=True))
+    # Create histogram
+    if isinstance(a_binned, pd.DataFrame):
+        a_melt = pd.melt(a_binned, value_name='bin', var_name='count')
+        data = pd.crosstab(a_melt['bin'], a_melt['count'])
+    else:
+        bins, counts = np.unique(a_binned, return_counts=True)
+        data = pd.DataFrame({'bin': bins, 'count': counts}).set_index('bin')
+
+    # Return Histogram object
+    return Histogram(data)
 
 
 if __name__ == '__main__':
     a = np.random.rand(100000)
-    print(ihist(a, 0.1).to_pandas(probability=True, count=False))
+    print(ihist(a, 0.1).to_frame(probability=True, count=False))
